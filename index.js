e91416e42df75cbdc1ab035c59ff616d79a17fc2
Arpita Mehta <arpita@macbook-air.lan>
2020-02-18 04:46:31 -0800

    added orginial content

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ function init(resultFromServer){
     let windspeedElement = $('#windSpeed');
     let cityHeader = $('#cityHeader');
     let weatherIcon = $('.weatherLogo');
+    let descriptionElement = $('.description');
     
     weatherIcon.attr('src', `https://openweathermap.org/img/wn/${resultFromServer.weather[0].icon}@2x.png`);
     weatherDescription.removeClass('hidden')
@@ -54,14 +55,42 @@ function init(resultFromServer){
     cityHeader.text('City: ' + resultFromServer.name);
     humidityElement.text('Humidity levels at '+ resultFromServer.main.humidity + '%');
 
+//if weather is between 70-80 degrees alert= have a beautiful sunny day. if weather is between 69-50 degreers alert = don't forget your sweater. If weather is below 50 alert bundle up its chilly outside. If weather is between 100-81 degrees alert don't forget sunscreen and a hat. It will be warm today
+    var temperature = resultFromServer.main.temp;
+    var text; 
+    
+    if(temperature >= 70 && temperature <=80) {
+        text = "Have a beautiful day!"
+        
+    } else if(temperature >= 50 && temperature <= 69) {
+        text= "Don't forget your sweater! "
+    } else if(temperature < 50) {
+        text = " Bundle up, its chilly outside!"
+        
+    } else  {
+        text = "Don't forget suncreen and hat! Its a warm day!"
+    }
+    descriptionElement.text(text)
+    
+
 }
 
+
 function isStateValid(stateTerm) {
-    return true
+    //get from the website list of state abbr and match what the user input to that list
+    //return true or false
+    var states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NH','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY']  
+    if(states.includes(stateTerm)){
+        return true;
+    } else {
+        return false
+    }
+    
 }
 
 function showInvalidStateMessage() {
-    
+    var error = new Error('Invalid state')
+    handleError(error)
 }
 
 $('#weatherForm').on('submit', (event) => {
@@ -74,8 +103,6 @@ $('#weatherForm').on('submit', (event) => {
     } else {
         showInvalidStateMessage()
     }
-    
-    
     })
 
 
